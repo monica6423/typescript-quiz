2a0be3f46cc01fd305f98fe3261e9a9b6bb8a923
monica6423 <monica6423@gmail.com>
2021-01-18 16:30:21 +0200

    check answer function and next question function

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,80 @@
 import React, {useState} from 'react';
 import QuestionCard from './components/QuestionCard'
+import { fetchQuizQuestions } from './API';
+import { Difficulty, QuestionState } from './API';
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [ loading, setLoading] = useState(false);
-  const [ questions, setQuestions] = useState([]);
+  const [ questions, setQuestions] = useState<QuestionState[]>([]);
   const [ number, setNumber] = useState(0);
-  const [ userAnswers, setUserAnswers] = useState([]);
+  const [ userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [ score, setScore] = useState(0);
-  const [ gameOver, setGameOver] = useState();
+  const [ gameOver, setGameOver] = useState(true);
+
+  console.log('fetch', fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startTrivia = async() => {
+    setLoading(true);
+    setGameOver(false);
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if(!gameOver) {
+      const answer = e.currentTarget.value;
+      //check anser againt correct answer
+      const correct = questions[number].correct_answer === answer;
+      if(correct) setScore(prev => prev + 1);
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      };
+      setUserAnswers((prev) => [...prev, answerObject])
+    }
   }
 
   const nextQuestion = () => {
+    const nextQuestion = number + 1;
 
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    }else{
+      setNumber(nextQuestion);
+    }
   }
 
   return (
     <div className="App">
       <h1>React Quiz</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
       <button className="start" onClick={startTrivia}>
         Start
-      </button>
-      <p className="score">Score:</p>
-      <p>Loading Question...</p>
+      </button>):null}
+
+      {!gameOver ? <p className="score">Score: {score}</p> : null}
+      {loading&&<p>Loading Question...</p>}
+      {!loading&&!gameOver &&( 
       <QuestionCard 
         questionNr={number+1}
         totalQuestions = {TOTAL_QUESTIONS}
@@ -38,10 +82,13 @@ const App = () => {
         answers={questions[number].answers}
         userAnswer={userAnswers? userAnswers[number]: undefined}
         callback={checkAnswer}
-      />
-      <button className="next" onClick={nextQuestion}>
-        Next Question
-      </button>
+      />)}
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1?(
+        <button className="next" onClick={nextQuestion}>
+           Next Question
+        </button>
+      ):null}
+   
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { AnswerObject } from '../App'
 type Props = {
     question: string;
     answers: string[];
     callback: any;
-    userAnswer: any;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -25,8 +25,8 @@ const QuestionCard: React.FC<Props> = ({
             <p dangerouslySetInnerHTML={{ __html: question}}></p>
             <div>
                 {answers.map(answer => (
-                    <div>
-                        <button disabled={userAnswer} onClick={callback}>
+                    <div key={answer}>
+                        <button disabled={userAnswer? true: false} onClick={callback} value={answer}>
                             <span dangerouslySetInnerHTML={{ __html: answer}}></span>
                         </button>
                     </div>
